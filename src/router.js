391eb6728768d65613e8a4375f929ebffdbaa7c4
草鞋没号 <308487730@qq.com>
2021-07-14 09:35:29 +0800

    chore: UPDATEW

--- src/router.js
+++ src/router.js
@@ -1,3 +1,8 @@
+import { DEFAULT_EXTENSIONS } from '../vite-plugins/utils'
+
+console.log(DEFAULT_EXTENSIONS)
+
+const dynamicVar = 'dynamic'
 
 export const routes = [
   {
@@ -7,5 +12,21 @@ export const routes = [
   {
     path: '/news',
     component: require('@/views/news.vue'),
+  },
+  {
+    path: '/dynamic',
+    component: () => {
+
+
+      const res = import(`@/views/${dynamicVar}`)
+      const res2 = import(`@/views/${dynamicVar}/index`)
+      const res3 = import(`@/views/${dynamicVar}` + '/index')
+      const res4 = import('@/views/dynamic')
+
+      console.log(res, res2, res3, res4)
+
+
+      return res
+    },
   },
 ]
---
+++ src/views/dynamic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="views-dynamic">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dynamic',
+  data() {
+    return {
+      msg: 'Dynamic Components',
+    }
+  },
+  created() {
+    console.log(this.msg)
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.views-dynamic {
+  color: aliceblue;
+}
+</style>
